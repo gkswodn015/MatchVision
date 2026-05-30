05b60d99dcc48e75b49df5bcf226169a8b978a03
안태현 <a32252554@dankook.ac.kr>
2026-05-30 18:26:17 +0900

    Connect all page navigation links

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('signup/', views.signup, name='signup'),
+
     path('upload/', views.upload_match, name='upload_match'),
     path('matches/', views.match_list, name='match_list'),
 
